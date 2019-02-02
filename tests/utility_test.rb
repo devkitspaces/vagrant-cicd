4f7d50fc4045e8caa6b72e792a93f89daaa4c0a2
jrbeverly <jrbeverly@live.com>
2019-02-02 11:52:54 -0500

    Fix formatting

--- tests/utility_test.rb
+++ tests/utility_test.rb
@@ -3,41 +3,41 @@ require 'palindrome'
 require 'awesome_print'
 
 class PalindromeTest < Test::Unit::TestCase
-    SAS_P = "sas"
+  SAS_P = "sas"
 
-    def setup
-    end
-
-    def test_p_match
-      res = Palindrome.p_match("esase", 1, 3)
-      assert(res.eql?("esase"), "Expected esase but was #{res}" )
-    end
-
-    def test_p_match_single
-      res = Palindrome.p_match("esasf",1, 3)
-      assert(res.eql?(SAS_P), "Expected #{SAS_P} but was #{res}" )
-    end
-
-    def test_find_all_none
-      pal_ary = Palindrome.find_all("etch")
-      assert(pal_ary.size == 0, "Expected 0 but size was " + pal_ary.size.to_s)
-    end
-
-    def test_find_all_one_beginning
-      pal_ary = Palindrome.find_all("sasit")
-      assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
-      assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
-    end
-
-    def test_find_all_one_end
-      pal_ary = Palindrome.find_all("zerosas")
-      assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
-      assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
-    end
-
-    def test_find_all_one_middle
-      pal_ary = Palindrome.find_all("zerosasend")
-      assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
-      assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
-    end
+  def setup
   end
+
+  def test_p_match
+    res = Palindrome.p_match("esase", 1, 3)
+    assert(res.eql?("esase"), "Expected esase but was #{res}" )
+  end
+
+  def test_p_match_single
+    res = Palindrome.p_match("esasf",1, 3)
+    assert(res.eql?(SAS_P), "Expected #{SAS_P} but was #{res}" )
+  end
+
+  def test_find_all_none
+    pal_ary = Palindrome.find_all("etch")
+    assert(pal_ary.size == 0, "Expected 0 but size was " + pal_ary.size.to_s)
+  end
+
+  def test_find_all_one_beginning
+    pal_ary = Palindrome.find_all("sasit")
+    assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
+    assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
+  end
+
+  def test_find_all_one_end
+    pal_ary = Palindrome.find_all("zerosas")
+    assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
+    assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
+  end
+
+  def test_find_all_one_middle
+    pal_ary = Palindrome.find_all("zerosasend")
+    assert(pal_ary.size == 1, "Expected 1 but size was " + pal_ary.size.to_s)
+    assert(pal_ary[0].eql?(SAS_P), "Expected #{SAS_P} but was " + pal_ary[0])
+  end
+end
